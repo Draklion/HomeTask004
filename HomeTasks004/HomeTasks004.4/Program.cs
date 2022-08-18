195ab78812dc1d244481c40fef3243a95756909f
«Dmitry_Machnev» <draklion56@gmail.com>
2022-08-18 23:08:19 +0500

    HomeTask004.4 completed.

--- HomeTasks004/HomeTasks004.4/Program.cs
+++ HomeTasks004/HomeTasks004.4/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string CW(string msg)
+{
+  Console.WriteLine(msg);
+  return msg;
+}
+string CR()
+{
+  return Console.ReadLine();
+}
+try
+{
+  CW("Задание 4. Программа, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.");
+
+  int firstNumber = Int32.Parse(CR());
+  int secondNumber = Int32.Parse(CR());
+  int numberDegree = 1;
+
+  for (int i = 0; i < secondNumber; i++)
+    numberDegree = numberDegree * firstNumber;
+
+  CW($"Число {firstNumber} в степени {secondNumber} = {numberDegree}");
+}
+catch { CW("Вы ввели некорректное значение."); }
