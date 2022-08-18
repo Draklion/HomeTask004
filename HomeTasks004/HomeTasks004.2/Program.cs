8ae65d0cd2e93bd07b5eff6c5453d7a751b9b64b
«Dmitry_Machnev» <draklion56@gmail.com>
2022-08-18 22:11:18 +0500

    HomeTask004.2 completed.

--- HomeTasks004/HomeTasks004.2/Program.cs
+++ HomeTasks004/HomeTasks004.2/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string CW(string msg)
+{
+  Console.WriteLine(msg);
+  return msg;
+}
+string CR()
+{
+  return Console.ReadLine();
+}
+try
+{
+  CW("Задание 2. Программа, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.");
+  CW("Введите число: ");
+  int number = Int32.Parse(CR());
+  int result = 1;
+  for (int i = 1; i <= number; i++)
+    result = i * result;
+  CW($"Произведение чисел от 1 до {number}: {result}");
+}
+catch { CW("Вы ввели некорректное значение."); }
