90afdd8ea9235b466669e1d8a174665f2c7cae88
«Dmitry_Machnev» <draklion56@gmail.com>
2022-08-18 23:34:53 +0500

    HomeTask004.5 completed.

--- HomeTasks004/HomeTasks004.5/Program.cs
+++ HomeTasks004/HomeTasks004.5/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string CW(string msg)
+{
+  Console.WriteLine(msg);
+  return msg;
+}
+string CR()
+{
+  return Console.ReadLine();
+}
+
+void sum(string number, out string numberstr)
+{
+  int sum = 0;
+  for (int i = 0; i < number.Length; i++)
+    sum = sum + Int32.Parse(number[i].ToString());
+  numberstr = sum.ToString();
+}
+
+try
+{
+  CW("Задание 5. Программа, которая принимает на вход число и выдаёт сумму цифр в числе.");
+  string number = CR();
+  string numberstr = "";
+  sum(number, out numberstr);
+
+  CW($"Cумму цифр в числе: {numberstr}");
+}
+catch { CW("Вы ввели некорректное значение."); }
