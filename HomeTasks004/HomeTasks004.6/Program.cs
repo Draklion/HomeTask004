41d62b359a900d88c50427ca6fd4f195701a5b32
«Dmitry_Machnev» <draklion56@gmail.com>
2022-08-18 23:39:35 +0500

    HomeTask004.6 completed.

--- HomeTasks004/HomeTasks004.6/Program.cs
+++ HomeTasks004/HomeTasks004.6/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string CW(string msg)
+{
+  Console.WriteLine(msg);
+  return msg;
+}
+try
+{
+  CW("Задание 6. Программа, которая задаёт массив из 8 элементов и выводит их на экран.");
+
+  int[] array = new int[8];
+  Random rand = new Random();
+
+  for (int i = 0; i < array.Length; i++)
+    array[i] = rand.Next(0, 100);
+
+  CW("[" + string.Join(",", array) + "]");
+}
+catch { CW("Вы ввели некорректное значение."); }
